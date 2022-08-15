c7561c0d5b330de5bb74e6a3aa69a78114c22170
AmerDawood <amermadawood@gmail.com>
2022-08-15 20:07:36 +0300

    update-chat

--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -15,107 +15,135 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {}, icon: const Icon(Iconsax.arrow_left_2)),
-                const SizedBox(
-                  width: 100,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: ClipOval(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 60,
+                left: 10,
+                right: 20
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        // Navigator.pushReplacementNamed(context, 'App_Screen');
+                      }, icon: const Icon(Iconsax.arrow_left_2)),
+
+                  ClipOval(
                     child: Image.asset(
                       'images/a1.png',
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 100,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: ClipOval(
-                    child: Image.asset(
-                      'images/face1.png',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const TextWidget(
-            text: 'Maria Lucero',
-            color: Color(0xff010A1B),
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const TextWidget(
-            text: 'Active',
-            color: Color(0xff6B7A9F),
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Iconsax.call, size: 40),
-                color: const Color(0xffAC8AF7),
-              ),
-              const SizedBox(
-                width: 25,
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Iconsax.video,
-                  color: Color(0xffAC8AF7),
-                  size: 40,
-                ),
-              ),
-            ],
-          ),
-          Container(
-            height: 520,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-            ),
-            child: Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 410),
-                    child: Container(
-                      color: Colors.white,
-                      child: TextFieldChat(),
+
+                  InkWell(
+                    onTap: () {
+                      // Navigator.pushReplacementNamed(context, '/profile_screen');
+                    },
+                    child: ClipOval(
+                      child: Image.asset(
+                        'images/face1.png',
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            const TextWidget(
+              text: 'Maria Lucero',
+              color: Color(0xff010A1B),
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const TextWidget(
+              text: 'Active',
+              color: Color(0xff6B7A9F),
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius:BorderRadius.circular(60),
+                      border: Border.all(color: Color(0xffAC8AF7),width: 3)
+                    ),
+                    child:
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Iconsax.call, size: 40),
+                      color: const Color(0xffAC8AF7),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        borderRadius:BorderRadius.circular(60),
+                        border: Border.all(color: Color(0xffAC8AF7),width: 3)
+                    ),
+                    child:
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Iconsax.video,
+                        color: Color(0xffAC8AF7),
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 410),
+                      child: Container(
+                        color: Colors.white,
+                        child: TextFieldChat(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/chat/component/text_filed_chat.dart
+++ lib/screens/chat/component/text_filed_chat.dart
@@ -10,17 +10,37 @@ class TextFieldChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(20.0),
-        child:TextField(
-          decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
+        child:Row(
+          children: [
+            Expanded(
+              flex: 7,
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    filled: true,
+                    suffixIcon: Icon(Icons.image,color: Colors.grey,),
+                    hintStyle: TextStyle(color: Colors.grey),
+                    hintText: "Write a reply",
+                    fillColor: Colors.white70
+                ),
               ),
-              filled: true,
-              suffixIcon: Icon(Iconsax.send1,color: Colors.grey,),
-              hintStyle: TextStyle(color: Colors.grey),
-              hintText: "Write a reply",
-              fillColor: Colors.white70),
-        )
+            ),
+
+            Expanded(
+                flex: 1,
+                child:Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Iconsax.send_1, size: 40),
+                    color: Colors.grey,
+                  ),
+                ),
+            )
+          ],
+        ),
     );
   }
 }
